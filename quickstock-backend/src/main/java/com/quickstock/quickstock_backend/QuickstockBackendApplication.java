069299e09a1bd970bd68a07b311eeed241bd4468
Cesar Rodriguez <59242247+Cesar001-co@users.noreply.github.com>
2025-01-29 23:25:00 -0500

    Add initial project structure with Maven configuration, application properties, and test setup

---
+++ quickstock-backend/src/main/java/com/quickstock/quickstock_backend/QuickstockBackendApplication.java
@@ -0,0 +1,13 @@
+package com.quickstock.quickstock_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickstockBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickstockBackendApplication.class, args);
+	}
+
+}
